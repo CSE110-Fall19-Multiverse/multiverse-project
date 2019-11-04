b7df0da003334695919ad5940650b7b9fb7ef6e7
AuFish <shenchg126@gmail.com>
2019-11-03 22:27:24 -0800

    workable firebase dependencies

--- App.js
+++ App.js
@@ -8,20 +8,13 @@ import Navigation from './navigation';
 import { Block } from './components';
 import Firebase, { FirebaseContext } from "./components/Firebase";
 
-// import all used images for cache
+// import all used images
 const images = [
-  //TODO: cache all newly uploaded images automatically (now hardcoded)
   require('./assets/images/illustration_1.png'),
   require('./assets/images/illustration_2.png'),
   require('./assets/images/illustration_3.png'),
   require('./assets/images/avatar.png'),
-  require('./assets/images/madeline.png'),
-  require('./assets/images/charles.png'),
-  require('./assets/images/cheng.png'),
-  require('./assets/images/gary.png'),
-  require('./assets/images/paul.png'),
-  require('./assets/images/jessica.png'),
-  require('./assets/icons/back.png'),
+  // TODO for all avi images (for caching)
 ];
 
 export default class App extends React.Component {
