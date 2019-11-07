29e37543bb6b54d5c9d412c23c9686cdee0327bf
Charles Yu <CharlesYu2000@gmail.com>
2019-11-07 15:00:13 -0800

    fixed the duplicate key thing

--- screens/Marketplace.js
+++ screens/Marketplace.js
@@ -77,6 +77,7 @@ class Marketplace extends Component {
 
       return (
         <TouchableOpacity
+            key={`view-${view}`}
             onPress={() => this.handleView(view)}
             style={[
                 styles.view, 
